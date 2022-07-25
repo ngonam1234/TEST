42b9fedbd98cd7446e8d693501fba1bf58186e93
ngohainam <ngohainam19@gmail.com>
2022-07-25 17:36:04 +0700

    delete

--- ConfigKey.js
+++
@@ -1,3 +0,0 @@
-import fs from 'fs'
-export const privateKEY = fs.readFileSync('./config/private.key', 'utf8');
-export const publicKEY = fs.readFileSync('./config/public.key', 'utf8');
--- constant/HttpResponseCode.js
+++
@@ -1,6 +0,0 @@
-export const OK = 200;
-export const Unauthorized = 401;
-export const BAD_REQUEST = 400;
-export const CREATED = 201;
-export const NO_CONTENT = 204;
-export const SYSTEM_ERROR = 500;
